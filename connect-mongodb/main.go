220c2b15bc0a22b3db3dca2a500a37951b1bf1a3
Wachira Duangdee <wcrcorp@gmail.com>
2022-03-09 16:07:13 +0700

    Create main.go

---
+++ connect-mongodb/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "time"
+
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+    /*
+            Connect to my cluster
+    */
+    client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://user:pass@172.16.1.5:27017/?authSource=admin&authMechanism=SCRAM-SHA-256"))
+    if err != nil {
+        log.Fatal(err)
+    }
+    ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+    err = client.Connect(ctx)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer client.Disconnect(ctx)
+
+      /*
+            List databases
+    */
+    databases, err := client.ListDatabaseNames(ctx, bson.M{})
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(databases)
+
+}
